1186b0941cc0bfc11d7bf55bfc58036a3856eb75
roberthitchens3 <rjhitchens3@gmail.com>
2016-02-02 11:12:33 -0500

    minor update

--- src/test/java/io/zipcoder/test/SpecInventoryManager.java
+++ src/test/java/io/zipcoder/test/SpecInventoryManager.java
@@ -3,6 +3,17 @@ package io.zipcoder.test;
 /**
  * Created by roberthitchens3 on 2/1/16.
  */
+import io.zipcoder.InventoryManager;
+import org.junit.Test;
+import static org.junit.Assert.*;
 public class SpecInventoryManager {
 
+    InventoryManager inman = new InventoryManager();
+    public void testUserInput() {
+        String input = "2.39 \n";
+        System.setIn(new java.io.ByteArrayInputStream(input.getBytes()));
+        String output = inman.userInput("enter a price: ", "number");
+        assertNotNull("returned value should not be null: ", output);
+        assertEquals("returned value should be equal to initial input value: ", "2.39", output);
+    }
 }
